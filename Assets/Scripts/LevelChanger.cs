4ef335ef155ebe1f87ca3d94d5235c1b4cc6a74a
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2016-06-17 00:19:46 -0400

    Made Test Level + Added Main Mechanics in

---
+++ Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelChanger : MonoBehaviour
+{
+    GameObject Escort;
+    void Awake()
+    {
+        Escort= GameObject.FindGameObjectWithTag("T_Escort");
+    }
+
+    void OnCollisionEnter (Collision goal)
+    {
+       if (Escort)
+        {
+            print("Goal!");
+        }
+    }
+}
