efecc0b6bbf5ca5477e6df3eb156366953662519
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2016-06-24 12:47:11 -0400

    Added Level 5, Added music and Main Menu!

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour
+{
+    static bool AudioBegin = false;
+    public AudioSource bgm;
+
+    void Awake()
+    {
+        if (!AudioBegin)
+        {
+            bgm.Play();
+            DontDestroyOnLoad(gameObject);
+            AudioBegin = true;
+        }
+    }
+}
