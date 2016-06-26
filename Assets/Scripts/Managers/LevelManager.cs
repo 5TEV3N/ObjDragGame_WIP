23dba1c39e4e7bc5dd0c18683e62208a35e5c8bd
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2016-06-25 21:57:24 -0400

    Finished UI = QuitGame? Yes or No

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -5,15 +5,24 @@ using UnityEngine.SceneManagement;
 public class LevelManager : MonoBehaviour
 {
     private LevelManager levelManager;
+    private UIManager UIManager;
 
     void Awake()
     {
         levelManager = GameObject.FindGameObjectWithTag("T_LevelManager").GetComponent<LevelManager>();
+        UIManager = GameObject.FindGameObjectWithTag("T_UIManager").GetComponent<UIManager>();
     }
-
     public void LoadLevel(string levelID)
     {
         SceneManager.LoadScene(levelID); //Loads scene according to name (Can be int ID if needed)
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            UIManager.UnhideUI();
+            print("Menu Activated");
+        }
     }
 
     public void LoadNextLevel()
---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject QuitGameText;
+    public GameObject YesButton;
+    public GameObject NoButton;
+
+    public void CloseGame()
+    {
+        Application.Quit();
+        print("Goodbye! Thanks for playing!");
+    }
+
+    public void UnhideUI()
+    {
+        QuitGameText.SetActive(true);
+        YesButton.SetActive(true);
+        NoButton.SetActive(true);
+    }
+
+    public void hideUI()
+    {
+        QuitGameText.SetActive(false);
+        YesButton.SetActive(false);
+        NoButton.SetActive(false);
+    }
+}
