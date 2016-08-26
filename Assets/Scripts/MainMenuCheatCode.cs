0eaa718a10aa39c8133c42ad0568d2cb7c8caad3
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2016-08-26 14:31:53 -0400

    Fixed level 4 , Started working on cheats
    Level 4
    - fixed wall and goal platform

---
+++ Assets/Scripts/MainMenuCheatCode.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuCheatCode : MonoBehaviour
+{
+    MainMenuCheatCode mainMenuCheatCode;
+    int cheatCounter = 0;
+
+	void Awake ()
+    {
+        mainMenuCheatCode = GameObject.FindGameObjectWithTag("T_MainMenuCheat").GetComponent<MainMenuCheatCode>();
+	}
+	
+	void Update ()
+    {
+	    if (Input.GetKeyDown(KeyCode.Delete))
+            {
+                cheatCounter++;
+                print(cheatCounter);
+
+                if (cheatCounter == 20)
+                {
+                    print(" C H E A T S  E N A B L E D");
+                    // ADD CHEATS IN HERE
+                }
+            }
+	}
+}
